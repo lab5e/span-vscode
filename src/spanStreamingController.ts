82774758c40b63f298920ba9a90ea6a7d9c1ed46
Per Kristian Kummermo <pkkummermo@lab5e.com>
2021-02-25 19:21:16 +0100

    refactor(spanStreamingController): extract methods

--- src/spanStreamingController.ts
+++ src/spanStreamingController.ts
@@ -12,62 +12,56 @@ export class SpanStreamingController {
     vscode.commands.registerCommand(
       "span-vscode.tailDeviceData",
       (node: DeviceTreeItem) => {
-        if (this.websocket) {
-          this.streamChannel?.appendLine(
-            "Closing connection to SPAN live data"
-          );
-          this.websocket.close();
-          this.websocket = null;
-        }
-
-        if (this.streamChannel) {
-          this.streamChannel.hide();
-          this.streamChannel.dispose();
-          this.streamChannel = null;
-        }
-
-        this.streamChannel = vscode.window.createOutputChannel(
-          `${node.tooltip} - Stream`
-        );
-        this.streamChannel.show();
-        this.streamChannel.appendLine("Connecting to SPAN live data");
-        this.websocket = new WebSocket(
-          `wss://api.lab5e.com/span/collections/${node.device.collectionId}/devices/${node.device.deviceId}/from`,
-          {
-            headers: {
-              // eslint-disable-next-line @typescript-eslint/naming-convention
-              "X-API-Token": this.spanToken,
-            },
-          }
-        );
-
-        this.websocket.on("open", () => {
-          this.streamChannel?.appendLine("Connected successfully");
-        });
-        this.websocket.on("message", (data) => {
-          const deviceMessage = JSON.parse(data.toString());
-          if (deviceMessage.type === "keepalive") {
-          } else {
-            this.streamChannel?.appendLine(
-              JSON.stringify(deviceMessage, null, 2)
-            );
-          }
-        });
+        this.closeStream();
+        this.initiateDeviceStream(node);
       }
     );
 
-    vscode.commands.registerCommand("span-vscode.closeDeviceStream", () => {
-      if (this.websocket) {
-        this.streamChannel?.appendLine("Closing connection to SPAN live data");
-        this.websocket.close();
-        this.websocket = null;
-      }
+    vscode.commands.registerCommand(
+      "span-vscode.closeDeviceStream",
+      this.closeStream
+    );
+  }
 
-      if (this.streamChannel) {
-        this.streamChannel.hide();
-        this.streamChannel.dispose();
-        this.streamChannel = null;
+  private initiateDeviceStream(deviceNode: DeviceTreeItem) {
+    this.streamChannel = vscode.window.createOutputChannel(
+      `${deviceNode.tooltip} - Stream`
+    );
+    this.streamChannel.show();
+    this.streamChannel.appendLine("Connecting to SPAN live data");
+    this.websocket = new WebSocket(
+      `wss://api.lab5e.com/span/collections/${deviceNode.device.collectionId}/devices/${deviceNode.device.deviceId}/from`,
+      {
+        headers: {
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          "X-API-Token": this.spanToken,
+        },
+      }
+    );
+
+    this.websocket.on("open", () => {
+      this.streamChannel?.appendLine("Connected successfully");
+    });
+    this.websocket.on("message", (data) => {
+      const deviceMessage = JSON.parse(data.toString());
+      if (deviceMessage.type === "keepalive") {
+      } else {
+        this.streamChannel?.appendLine(JSON.stringify(deviceMessage, null, 2));
       }
     });
   }
+
+  private closeStream = () => {
+    if (this.websocket) {
+      this.streamChannel?.appendLine("Closing connection to SPAN live data");
+      this.websocket.close();
+      this.websocket = null;
+    }
+
+    if (this.streamChannel) {
+      this.streamChannel.hide();
+      this.streamChannel.dispose();
+      this.streamChannel = null;
+    }
+  };
 }
